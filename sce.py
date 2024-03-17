fb3ff2fa4d7fb0e8a89d36162f5b92a61cb517a8
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-03-17 15:15:15 +0530

    Update sce.py

--- sce.py
+++ sce.py
@@ -44,20 +44,20 @@ def stop_scheduling(stop_time):
     print(f"Scheduling stopped at {stop_time}")
 
 # # Main function
-# if __name__ == "__main__":
-#     start_time = "14:11"  # Specify the start time
-#     stop_time = "14:13"   # Specify the stop time
+if __name__ == "__main__":
+    start_time = "15:16"  # Specify the start time
+    stop_time = "14:17"   # Specify the stop time
     
-#     start_scheduling(start_time)
+    start_scheduling(start_time)
     
-#     # Keep the program running until the stop time is reached
-#     while True:
-#         current_time = time.strftime("%H:%M", time.localtime())
-#         if current_time >= stop_time:
-#             stop_scheduling(current_time)
-#             break
-#         schedule.run_pending()
-#         time.sleep(1)
+    # Keep the program running until the stop time is reached
+    while True:
+        current_time = time.strftime("%H:%M", time.localtime())
+        if current_time >= stop_time:
+            stop_scheduling(current_time)
+            break
+        schedule.run_pending()
+        time.sleep(1)
  
 # # Task scheduling
 # # After every 10mins geeks() is called. 
