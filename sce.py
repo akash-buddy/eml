6906cc3b82d6a52f38beab053da501468951b222
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2024-03-19 14:51:07 +0530

    Update sce.py

--- sce.py
+++ sce.py
@@ -18,46 +18,46 @@ dg='hdfc-bank-ltd'
 st.markdown(f'[Click here to go to another page](https://groww.in/charts/stocks/{dg}?exchange=NSE)')
 
 
+col1,col2=st.columns(2)
 
 
 
+# def send_email(body):
+#     sender_email = "akashemail707@gmail.com"
+#     receiver_email = "akash7000a@gmail.com"
+#     password = "jzzg djgs jwzc daeh"
+#     server=smtplib.SMTP("smtp.gmail.com", 587)
+#     server.starttls()
+#     server.login(sender_email, password)
+#     st.write("login successful")
+#     server.sendmail(sender_email, receiver_email,body)
+#     server.quit()
 
-def send_email(body):
-    sender_email = "akashemail707@gmail.com"
-    receiver_email = "akash7000a@gmail.com"
-    password = "jzzg djgs jwzc daeh"
-    server=smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(sender_email, password)
-    st.write("login successful")
-    server.sendmail(sender_email, receiver_email,body)
-    server.quit()
+# def task():
+#     mess="Performing the task..."
+#     send_email(mess)
 
-def task():
-    mess="Performing the task..."
-    send_email(mess)
+# # Function to start scheduling
+# def start_scheduling(start_time):
+#     schedule.every().day.at(start_time).do(task)  # Schedule the task to start at the specified time
+#     st.write(f"Scheduling started at {start_time}")
 
-# Function to start scheduling
-def start_scheduling(start_time):
-    schedule.every().day.at(start_time).do(task)  # Schedule the task to start at the specified time
-    st.write(f"Scheduling started at {start_time}")
-
-# Function to stop scheduling
-def stop_scheduling(stop_time):
-    schedule.clear()  # Clear all scheduled tasks
-    st.write(f"Scheduling stopped at {stop_time}")
-
-
-start_time = st.text_input('start_time', '09:15') 
-stop_time = st.text_input('stop_time', '09:16')
+# # Function to stop scheduling
+# def stop_scheduling(stop_time):
+#     schedule.clear()  # Clear all scheduled tasks
+#     st.write(f"Scheduling stopped at {stop_time}")
 
 
-if st.button('Say hello'):
-    start_scheduling(start_time)
-    while True:
-        current_time = time.strftime("%H:%M", time.localtime())
-        if current_time >= stop_time:
-            stop_scheduling(current_time)
-            break
-        schedule.run_pending()
-        time.sleep(1)
+# start_time = st.text_input('start_time', '09:15') 
+# stop_time = st.text_input('stop_time', '09:16')
+
+
+# if st.button('Say hello'):
+#     start_scheduling(start_time)
+#     while True:
+#         current_time = time.strftime("%H:%M", time.localtime())
+#         if current_time >= stop_time:
+#             stop_scheduling(current_time)
+#             break
+#         schedule.run_pending()
+#         time.sleep(1)
